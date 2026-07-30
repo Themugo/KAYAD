65be4b4b9e8493f822d25949ba0d2e237f53a57b
James Mugo <jimmythemugo@gmail.com>
2026-07-30 09:59:09 +0300

    cleanup: delete duplicate old barrel files and 26 stale .jsx components

--- src/components/ui/Tooltip.jsx
+++
@@ -1,8 +0,0 @@
-export default function Tooltip({ text, children, className = '' }) {
-  return (
-    <div className={`ui-tooltip-wrapper ${className}`} style={{ position: 'relative', display: 'inline-flex' }}>
-      {children}
-      <span className="ui-tooltip" role="tooltip">{text}</span>
-    </div>
-  );
-}
