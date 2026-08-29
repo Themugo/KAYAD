09fa0a383a4a87a18a1c71ce012a8867bc761b7d
James Mugo <jimmythemugo@gmail.com>
2026-08-29 08:53:40 +0300

    Legacy cleanup: remove 240 dead files, fix test collision

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -4,4 +4,3 @@ export { default as AdminSidebar } from './AdminSidebar';
 export { default as DealerLayout } from './DealerLayout';
 export { default as DealerSidebar } from './DealerSidebar';
 export { default as Footer } from './Footer';
-export { default as MobileBottomNav } from './MobileBottomNav';
